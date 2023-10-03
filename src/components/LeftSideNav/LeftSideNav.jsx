5c4c43a6c7935c3a666b909b5a93fbebd8627fba
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-03 18:55:37 +0600

    leftisde navbar done

--- src/components/LeftSideNav/LeftSideNav.jsx
+++ src/components/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,59 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import leftImg1 from "../../assets/1.png"
+import leftImg2 from "../../assets/2.png"
+import leftImg3 from "../../assets/3.png"
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('../../../public/categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+
+    }, []);
+    console.log(categories);
+
     return (
-        <div>
-            <h2 className="text-2xl">This is Left Side Nav</h2>
+        <div className="space-y-6">
+            <h2 className="text-xl font-semibold">All Categories</h2>
+            {
+                categories.map(category => <NavLink to={`category/${category.id}`} className="block ml-4 text-xl font-semibold  mx-auto" key={category.id}>{category.name} </NavLink>)
+            }
+            <div>
+                <img src={leftImg1} alt="" />
+                <h3 className="text-xl">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                <div className="py-2">
+                    <p className="inline-flex gap-1">Sports  <span className="ml-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75" stroke="#9F9F9F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </span>Jan 4, 2022</p>
+                </div>
+            </div>
+            <div>
+                <img src={leftImg2} alt="" />
+                <h3 className="text-xl">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                <div className="py-2">
+                    <p className="inline-flex gap-1">Sports  <span className="ml-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75" stroke="#9F9F9F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </span>Jan 4, 2022</p>
+                </div>
+            </div>
+            <div>
+                <img src={leftImg3} alt="" />
+                <h3 className="text-xl">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                <div className="py-2">
+                    <p className="inline-flex gap-1">Sports  <span className="ml-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75" stroke="#9F9F9F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </span>Jan 4, 2022</p>
+                </div>
+            </div>
         </div>
     );
 };
