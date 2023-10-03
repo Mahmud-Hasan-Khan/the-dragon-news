1e61d77b981e4a9ca5b5080a0c11317712b7593f
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-03 08:52:19 +0600

    header section done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="flex items-center justify-center text-center mx-2 md:mx-auto">
             <div>
                 <img src={logo} alt="" />
-                <p className="text-lg mt-1">Journalism Without Fear or Favour</p>
+                <p className="text-lg mt-1 text-[#706F6F]">Journalism Without Fear or Favour</p>
                 <p className=" text-xl font-medium text-[#403F3F] ">{moment().format("dddd")}<span className="text-[#706F6F]"> {moment().format("MMMM D, YYYY")}</span></p>
 
             </div>
